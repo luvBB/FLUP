aa93d186d9d8b4171f1759cb971d2074d4fead09
Cristi <108606727+luvBB@users.noreply.github.com>
2024-09-01 09:57:29 +0300

    Update Movie.py
    Config moved to config.py

--- Movie.py
+++ Movie.py
@@ -6,26 +6,10 @@ import requests
 import re
 import sys
 from bs4 import BeautifulSoup
+from config import *
 
 # ===============================
-# Configuration: API Keys, Paths, and Credentials
-# ===============================
-img4k_api_url = 'https://img4k.net/api/1/upload'
-img4k_api_key = 'img4k.net api key'
-
-filelist_username = '*'
-filelist_password = '*'
-
-qbittorrent_url = 'http://localhost:8089'
-qbittorrent_username = '*'
-qbittorrent_password = '*'
-
-mediainfo_path = r"F:\FLUP\mediainfo\mediainfo.exe"
-ffmpeg_path = r"ffmpeg"
-
-
-# ===============================
-# Function Definitions
+# Let's Go!
 # ===============================
 
 # Function to delete specific files from the current directory.
