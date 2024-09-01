59a5bec9a377a8f106e531efacd4cf86d5fccb0f
Cristi <108606727+luvBB@users.noreply.github.com>
2024-09-01 10:04:10 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+# Config file for FLUP
+
+# img4k.net or any Chevereto site
+img4k_api_url = 'https://img4k.net/api/1/upload'
+img4k_api_key = 'INSERT-API-HERE'
+
+# TVDB
+api_key = 'INSERT-API-HERE'
+pin = 'INSERT-PIN-HERE'
+
+# FileList.io
+filelist_username = 'INSERT-USERNAME-HERE'
+filelist_password = 'INSERT-PASSWORD-HERE'
+
+# qBittorrent app
+qbittorrent_url = 'http://localhost:8089'
+qbittorrent_username = 'INSERT-USERNAME-HERE'
+qbittorrent_password = 'INSERT-PASSWORD-HERE'
+
+# Apps paths (change them to match your paths)
+mediainfo_path = r"F:\FLUP-main\mediainfo\mediainfo.exe"
+ffmpeg_path = r"ffmpeg"
+bdinfo_path = r"F:\FLUP-main\bdinfo\BDInfo.exe"
+vlc_path = r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
